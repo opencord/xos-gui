a338531beb36751719f90d15a44b4ac4be8b0f33
Matteo Scandolo <matteo.scandolo@gmail.com>
2017-05-10 09:52:36 -0700

    [CORD-1232] Filtering events based on updated properies
    Change-Id: If96da2f5c987f487b4d8b9ec7c68e44869cb4e12
    (cherry picked from commit e015fa919bede8603079aa6cca04cba7eb438686)

--- conf/proxy.js
+++ conf/proxy.js
@@ -3,21 +3,34 @@ const httpProxy = require('http-proxy');
 const target = process.env.PROXY || '192.168.46.100';
 
 const proxy = httpProxy.createProxyServer({
-  target: `http://${target}:9101`
+  target: `http://${target}`
 });
 
 const extensionsProxy = httpProxy.createProxyServer({
   target: `http://${target}/xos/`
 });
 
+const socketProxy = httpProxy.createProxyServer({
+  target: `http://${target}/`
+});
+
 proxy.on('error', function(error, req, res) {
-  res.writeHead(500, {
-    'Content-Type': 'text/plain'
-  });
+  res.writeHead(500, {'Content-Type': 'text/plain'});
   console.error('[Proxy]', error);
+});
+
+extensionsProxy.on('error', function(error, req, res) {
+  res.writeHead(500, {'Content-Type': 'text/plain'});
+  console.error('[extensionsProxy]', error);
+});
+
+socketProxy.on('error', function(error, req, res) {
+  res.writeHead(500, {'Content-Type': 'text/plain'});
+  console.error('[socketProxy]', error);
 });
 
 module.exports = {
   proxy,
-  extensionsProxy
+  extensionsProxy,
+  socketProxy
 };
